243498dc137c0d03ff129a4bc27bfb9bc98a996b
Sahil Mahendrakar <sahilmahendrakar@Sahils-MacBook-Pro.local>
2020-10-24 13:31:52 -0400

    Added navigation between Welcome and Login Screens

--- App.js
+++ App.js
@@ -3,8 +3,20 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import LoginScreen from './screens/LoginScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <WelcomeScreen/>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={WelcomeScreen} options={ {headerShown: false }}/>
+        <Stack.Screen name="Login" component={LoginScreen} options={ {headerShown: false }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-function WelcomeScreen(props) {
+function WelcomeScreen({navigation}) {
     return (
         <View style={styles.container}>
       <Image style={styles.image} source={require('../assets/PlantIcon.png')} />
       <Text style={styles.text}>PlantPals</Text>
 
-      <TouchableOpacity activeOpacity={0.8} style={{top:50}} onPress = {() => {console.log("hi")}}>
+      <TouchableOpacity activeOpacity={0.8} style={{top:50}} onPress = {() => navigation.navigate('Login')}>
         <View style = {{backgroundColor: '#424141', alignItems: 'center', 
                     justifyContent: 'center', borderRadius: 30, paddingRight:30, paddingLeft:30, paddingVertical:10}}
            > 
