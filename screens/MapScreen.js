4c2ac9eac117f1a03adc4824a884e2f4b32e18b5
ellenguo6 <ellenguo6@gmail.com>
2020-10-24 14:16:18 -0700

    maps update

---
+++ screens/MapScreen.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Marker } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import { StyleSheet, Text, View, SafeAreaView, Dimensions, Image, TouchableOpacity } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView 
+            style={styles.mapStyle}
+            initialRegion={{
+                latitude: 33.7872131,
+                longitude: -84.381931,
+                latitudeDelta: .005,
+                longitudeDelta: .005
+            }} >
+            <MapView.Marker
+                coordinate={{ latitude: 33.7872131, longitude: -84.381931 }}
+                title='Flatiron School Atlanta'
+                description='This is where the magic happens!'>
+
+                <Image source={require('../assets/PlantIcon.png')} style={styles.icon} />
+            </MapView.Marker>
+        </MapView>
+            
+        <View style={styles.mapHeader}>
+            {/* <SafeAreaView style={styles.mapHeader}> */}
+            <Text style={styles.headerText}>
+                Find a Plant! 
+            </Text>
+            {/* </SafeAreaView> */}
+        </View>
+
+        <TouchableOpacity 
+            style = {styles.button} 
+            title = "Click me" 
+            onPress={() => console.log("button pressed")}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  button: {
+    // flexDirection:'row',
+    position:'absolute',
+    // bottom: 10,
+    // alignSelf: "center",
+    backgroundColor: "darkgreen",
+    // borderWidth: 0.5,
+    // borderRadius: 20,
+    // width: 200,
+    // height: 200,
+  },
+  headerText: {
+    color: "#fff",
+    fontSize: 36,
+    top: 15,
+  },
+  icon: {
+    width: 50,
+    height: 50,
+    tintColor: 'darkgreen',
+
+  },
+  mapStyle: {
+    backgroundColor: "black",
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  mapHeader: {
+    backgroundColor: '#73db8d',
+    height: 100,
+    width: Dimensions.get('window').width,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    alignSelf:  "baseline"
+  }
+});
