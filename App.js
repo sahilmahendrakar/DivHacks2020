bc651b08e6cc4d59509a6be354f6f349a5afdb6e
Sahil Mahendrakar <sahilmahendrakar@Sahils-MacBook-Pro.local>
2020-10-24 02:28:36 -0400

    Welcome Screen + Login Screen

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import LoginScreen from './screens/LoginScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+
+export default function App() {
+  return <WelcomeScreen/>;
+}
+
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image, StatusBar } from 'react-native';
+
+function WelcomeScreen(props) {
+    return (
+        <View style={styles.container}>
+      <Image style={styles.image} source={require('../assets/PlantIcon.png')} />
+      <Text style={styles.text}>PlantPals</Text>
+
+      <TouchableOpacity activeOpacity={0.8} style={{top:50}} onPress = {() => {console.log("hi")}}>
+        <View style = {{backgroundColor: '#424141', alignItems: 'center', 
+                    justifyContent: 'center', borderRadius: 30, paddingRight:30, paddingLeft:30, paddingVertical:10}}
+           > 
+        <Text style = {{color: 'white', fontWeight:'bold'}}>Login</Text> 
+    </View>
+</TouchableOpacity> 
+      <StatusBar style="auto" />
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#73db8d',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text: {
+      fontSize: 50,
+      fontWeight: "bold",
+      color: "white"
+    },
+    image: {
+      top: -50,
+      width: 150,
+      height: 150,
+      tintColor: '#fff',
+      resizeMode: 'contain',
+    }
+  });
+  
+
+export default WelcomeScreen;
